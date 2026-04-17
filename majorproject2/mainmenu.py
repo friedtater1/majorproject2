15cf788be7999942be3442e30d961c0d7e7c2df6
friedtater1 <keira482@gmail.com>
2026-04-17 11:45:06 -0400

    Add files via upload

--- majorproject2/mainmenu.py
+++ majorproject2/mainmenu.py
@@ -1,11 +1,21 @@
 import pygame
+
+
 pygame.init()
 
-screen1=pygame.display.set_mode((1200,1200))
-backing=pygame.image.load("components/backdrop.jpeg").convert()
-title=pygame.image.load("components/titlecopy.png").convert()
-title=pygame.transform.scale(title,(200,200))
-backing=pygame.transform.scale(backing,(1000,1000))
+screen1=pygame.display.set_mode((1200,1000))
+title=pygame.image.load("components/titlecopy.png")
+title=pygame.transform.scale(title,(600,200))
+bu1=pygame.image.load("components/play.jpeg").convert_alpha()
+bu2=pygame.image.load("components/quit.jpeg").convert_alpha()
+bu1=pygame.transform.scale(bu1,(450,200))
+bu2=pygame.transform.scale(bu2,(450,200))
+
+pygame.mixer.music.load("music/elevator.mp3")
+pygame.mixer.music.play(loops=-1)
+
+hitsound=pygame.mixer.Sound("music/vine.mp3")
+
 
 class button(pygame.sprite.Sprite):
     def __init__(self,image,spawn):
@@ -14,7 +24,13 @@ class button(pygame.sprite.Sprite):
         self.spawn=spawn
         self.rect=self.image.get_rect(center=spawn)
 
+s1=button(bu1,(600,350))
+s2=button(bu2,(600,550))
+slis=pygame.sprite.Group()
 
+slis.add(s1)
+slis.add(s2)
+colour=(255,255,255)
 
 gameactive = True
 while gameactive:
@@ -23,11 +39,13 @@ while gameactive:
                 gameactive = False
         elif event.type== pygame.MOUSEBUTTONDOWN:
              if event.button==1:
-                  print("its working")
-             elif event.button==2:
-                gameactive = False
-                  
-    screen1.blit(title,(200,200))
-    screen1.blit(backing,(0,0))
+                  print("insert vinebooms here")
+             elif event.button==3:
+                gameactive= False
+    
+    
+    screen1.fill(colour)
+    screen1.blit(title,(350,100))
+    slis.draw(screen1)
     pygame.display.update()
     pygame.time.Clock().tick(60)
